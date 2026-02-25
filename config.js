38987de4221367140b4395b0c64e4b7b95e2ea93
mo <mam283887@gmail.com>
2026-02-25 05:33:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
-﻿// الحسابات الأساسية التي لا تُحذف أبداً (تقدر تضيف القيادات هنا)
+﻿// ==========================================// الحسابات الأساسية التي لا تُحذف أبداً (تقدر تضيف القيادات هنا)
 const ConfigUsers = [
     { user: "admin", pass: "123", name: "dev", rank: "Minister of Interior" },
-    { user: "L.S.P.D Chief", pass: "123", name: "أحمد الفهد", rank: "L.S.P.D Chief" },
+    { user: "ia9q", pass: "123", name: "محمد المطيري", rank: "Minister of Interior" },
+    { user: "u1kx", pass: "123", name: "عوده الاحمري", rank: "Minister of Interior" },
+    { user: "2zcp", pass: "123", name: "أحمد الفهد", rank: "Minister of Interior" },
+    { user: "an9r", pass: "123", name: "اليكس تكسر", rank: "A. Minister of Interior" },
+    { user: "u1kx", pass: "123", name: "عوده الاحمري", rank: "Minister of Interior" },
+    { user: "ah.46", pass: "123", name: "مشبب العتيبي", rank: "Captain" },
+    { user: "fzzzl2", pass: "123", name: "دانيل سميث", rank: "Lieutenant" },
+    { user: "9fsl", pass: "123", name: "فيصل بن سلمان", rank: "Lieutenantr" },
 ];
-
 // رتب الضباط والقيادة العليا - لهم صلاحيات كاملة (إنشاء حسابات، قبول مهام)
 const OfficerRanks = [
     "Minister of Interior",
@@ -15,6 +21,7 @@ const OfficerRanks = [
     "Cheif of SASP",
     "Deputy Cheif of SASP",
     "Advisor Cheif of SASP",
+    "Cheif of M.I.F",
     "Colonel",
     "Major",
     "Captain",
@@ -35,3 +42,16 @@ const EnlistedRanks = [
     "Solo Cadet", 
     "Cadet"
 ];
+
+
+
+
+
+
+
+
+
+
+
+
+
