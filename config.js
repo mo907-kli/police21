8e73d5f695866556f26fb31215030c7f7d138730
mo <mam283887@gmail.com>
2026-03-02 08:53:09 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,6 @@
 ﻿// ==========================================// الحسابات الأساسية التي لا تُحذف أبداً (تقدر تضيف القيادات هنا)
 const ConfigUsers = [
-    { user: "admin", pass: "123", name: "dev", rank: "Minister of Interior" },
-    { user: "ia9q", pass: "123", name: "محمد المطيري", rank: "Minister of Interior" },
-    { user: "a7yg", pass: "123", name: "ايدلر سميث", rank: "Minister of Interior" },
-    { user: "u1kx", pass: "123", name: "عوده الاحمري", rank: "Minister of Interior" },
-    { user: "2zcp", pass: "123", name: "أحمد الفهد", rank: "Minister of Interior" },
-    { user: "an9r", pass: "123", name: "اليكس تكسر", rank: "A. Minister of Interior" },
-    { user: "ah.46", pass: "123", name: "مشبب العتيبي", rank: "Captain" },
-    { user: "9fsl", pass: "123", name: "فيصل بن سلمان", rank: "Lieutenant" },
-    { user: "2ii6", pass: "123", name: "فيكاريو اسلا", rank: "Deputy L.S.P.D Chief" },
-    { user: "mo", pass: "123", name: "dev", rank: "Cadet" },
-    { user: "tav.l7", pass: "123", name: "تاف مارفل", rank: "Lieutenant" },
-    
+   
 ];
 // رتب الضباط والقيادة العليا - لهم صلاحيات كاملة (إنشاء حسابات، قبول مهام)
 const OfficerRanks = [
@@ -41,6 +30,7 @@ const EnlistedRanks = [
     "Solo Cadet", 
     "Cadet"
 ];
+
 
 
 
